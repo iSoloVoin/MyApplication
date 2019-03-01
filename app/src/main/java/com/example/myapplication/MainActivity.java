f6442f5661e4d602d5a89462367491b7d145f4c9
Denis Pimkin <denisbums@gmail.com>
2019-03-01 14:22:30 +0300

    patch 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = (Button) findViewById(R.id.goodbutton); //comment
+        mButton = (Button) findViewById(R.id.goodbutton); //comment123
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
