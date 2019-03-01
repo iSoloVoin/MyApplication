34b436fc223dd909880b28d3f04a3af23f6c9fd0
Denis Pimkin <denisbums@gmail.com>
2019-03-01 17:24:53 +0300

    Merge remote-tracking branch 'remotes/origin/Dima' into Denis
    # Conflicts:
    #	app/src/main/java/com/example/myapplication/MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = (Button) findViewById(R.id.goodbutton); //commentBLABLABLA
+        mButton = (Button) findViewById(R.id.goodbutton); //commentBLABLABLA 2
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
